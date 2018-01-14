b974c43cba1cbe218f90823bb6625985068f7634
Douglas G. Moore <doug@dglmoore.com>
2018-01-14 15:07:55 -0700

    Implement and test the Token class
    This class provides a basic type for representing tokens in a
    Chemevolve configuration file.

--- chemevolve/Lexer.py
+++ chemevolve/Lexer.py
@@ -38,3 +38,97 @@ class TokenType(Enum):
         '''
         return 'TokenType.{}'.format(self.name)
 
+class Token(object):
+    '''
+    The `Token` class provides basic 
+    '''
+    def __init__(self, type, data):
+        '''
+        Initialize a token from a `TokenType` and the lexed string
+        '''
+        if not isinstance(type, TokenType):
+            raise TypeError('type argument must be a TokenType')
+        elif not isinstance(data, str):
+            raise TypeError('data argument must be a string')
+
+        self.type = type
+        self.data = data
+
+    def convert(self):
+        '''
+        Convert a token to a python value appropriate for it's `TokenType`.
+        '''
+        if self.type == TokenType.INTEGER:
+            return int(self.data)
+        elif self.type == TokenType.FLOAT:
+            return float(self.data)
+        else:
+            return self.data
+
+    def validate(self):
+        '''
+        Raise a `ValueError` if the data is incompatible with the `TokenType`.
+        '''
+        def guard(expect, msg=None):
+            if self.data == expect:
+                pass
+            else:
+                if msg:
+                    msg = '{}, found {}'.format(msg, repr(self.data))
+                else:
+                    msg = 'expected {}, found {}'.format(repr(expect), repr(self.data))
+                raise ValueError(msg)
+
+        if self.type == TokenType.NL:
+            guard('\n')
+        elif self.type == TokenType.LT:
+            guard('<')
+        elif self.type == TokenType.GT:
+            guard('>')
+        elif self.type == TokenType.EQ:
+            guard('=')
+        elif self.type == TokenType.PLUS:
+            guard('+')
+        elif self.type == TokenType.MINUS:
+            guard('-')
+        elif self.type == TokenType.DASH:
+            guard('--')
+        elif self.type == TokenType.ARROW:
+            guard('->')
+        elif self.type == TokenType.OBRACKET:
+            guard('[')
+        elif self.type == TokenType.CBRACKET:
+            guard(']')
+        elif self.type == TokenType.OPAREN:
+            guard('(')
+        elif self.type == TokenType.CPAREN:
+            guard(')')
+        elif self.type == TokenType.COMMA:
+            guard(',')
+        elif self.type == TokenType.INTEGER:
+            try:
+                self.convert()
+            except ValueError:
+                guard(None, 'expected an integer value')
+        elif self.type == TokenType.FLOAT:
+            try:
+                self.convert()
+            except ValueError:
+                guard(None, 'expected a floating-point value')
+        elif self.type == TokenType.STRING:
+            pass
+        else:
+            raise ValueError('unexpected type/data combination encountered')
+
+    def __str__(self):
+        '''
+        Convert a `Token` into a human-readable string.
+        '''
+        return '({}, {})'.format(self.type, repr(self.data))
+
+    def __repr__(self):
+        '''
+        Create a python-evaluable string representation of a `Token`.
+        '''
+        return 'Token({},{})'.format(repr(self.type), repr(self.data))
+
--- test/test_lexer.py
+++ test/test_lexer.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 import unittest
-from chemevolve.Lexer import TokenType
+from chemevolve.Lexer import TokenType, Token
 
 class TestTokenType(unittest.TestCase):
     '''
@@ -50,3 +50,239 @@ class TestTokenType(unittest.TestCase):
         self.assertEqual('TokenType.FLOAT',    repr(TokenType.FLOAT))
         self.assertEqual('TokenType.STRING',   repr(TokenType.STRING))
 
+class TestToken(unittest.TestCase):
+    '''
+    Ensure that all is well with the `Token` class.
+    '''
+    def test_invalid_args(self):
+        '''
+        Token should be constructed from a TokenType and a string.
+        '''
+        with self.assertRaises(TypeError):
+            Token(0, '\n')
+        with self.assertRaises(TypeError):
+            Token(TokenType.NL, 5)
+
+        Token(TokenType.NL, '\n')
+
+    def test_convert_integer(self):
+        '''
+        Ensure that tokens with integer type can be properly converted to python
+        integers.
+        '''
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, '').convert()
+
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, '1.0').convert()
+
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, 'a').convert()
+
+        self.assertEqual(1, Token(TokenType.INTEGER, '1').convert())
+        self.assertEqual(1, Token(TokenType.INTEGER, '+1').convert())
+        self.assertEqual(-1, Token(TokenType.INTEGER, '-1').convert())
+
+    def test_convert_float(self):
+        '''
+        Ensure that tokens with float type can be properly converted to python
+        floats.
+        '''
+        with self.assertRaises(ValueError):
+            Token(TokenType.FLOAT, '').convert()
+
+        with self.assertRaises(ValueError):
+            Token(TokenType.FLOAT, 'a').convert()
+
+        self.assertEqual(1.0, Token(TokenType.FLOAT, '1').convert())
+        self.assertEqual(1.0, Token(TokenType.FLOAT, '+1').convert())
+        self.assertEqual(-1.0, Token(TokenType.FLOAT, '-1').convert())
+
+        self.assertEqual(100.0, Token(TokenType.FLOAT, '1e2').convert())
+        self.assertEqual(100.0, Token(TokenType.FLOAT, '+1e2').convert())
+        self.assertEqual(-100.0, Token(TokenType.FLOAT, '-1e2').convert())
+
+        self.assertEqual(100.0, Token(TokenType.FLOAT, '1e+2').convert())
+        self.assertEqual(100.0, Token(TokenType.FLOAT, '+1e+2').convert())
+        self.assertEqual(-100.0, Token(TokenType.FLOAT, '-1e+2').convert())
+
+        self.assertEqual(0.01, Token(TokenType.FLOAT, '1e-2').convert())
+        self.assertEqual(0.01, Token(TokenType.FLOAT, '+1e-2').convert())
+        self.assertEqual(-0.01, Token(TokenType.FLOAT, '-1e-2').convert())
+
+    def test_convert_others(self):
+        '''
+        Ensure that all stringy token types can be converted to strings.
+        '''
+        self.assertEqual('\n', Token(TokenType.NL, '\n').convert())
+        self.assertEqual('<',  Token(TokenType.LT, '<').convert())
+        self.assertEqual('>',  Token(TokenType.GT, '>').convert())
+        self.assertEqual('=',  Token(TokenType.EQ, '=').convert())
+        self.assertEqual('+',  Token(TokenType.PLUS, '+').convert())
+        self.assertEqual('-',  Token(TokenType.MINUS, '-').convert())
+        self.assertEqual('--', Token(TokenType.DASH, '--').convert())
+        self.assertEqual('->', Token(TokenType.ARROW, '->').convert())
+        self.assertEqual('[',  Token(TokenType.OBRACKET, '[').convert())
+        self.assertEqual(']',  Token(TokenType.CBRACKET, ']').convert())
+        self.assertEqual('(',  Token(TokenType.OPAREN, '(').convert())
+        self.assertEqual(')',  Token(TokenType.CPAREN, ')').convert())
+        self.assertEqual(',',  Token(TokenType.COMMA, ',').convert())
+
+        self.assertEqual('', Token(TokenType.STRING, '').convert())
+        self.assertEqual('apple', Token(TokenType.STRING, 'apple').convert())
+        self.assertEqual('+', Token(TokenType.STRING, '+').convert())
+
+    def test_validate_fails(self):
+        '''
+        Ensure that validate raises a `ValueError` when the token's data has
+        an invalid value.
+        '''
+        with self.assertRaises(ValueError):
+            Token(TokenType.NL, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.LT, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.GT, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.EQ, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.PLUS, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.MINUS, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.DASH, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.ARROW, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.OBRACKET, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.CBRACKET, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.OPAREN, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.CPAREN, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.COMMA, '').validate()
+
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, '1.0').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.INTEGER, 'a').validate()
+
+        with self.assertRaises(ValueError):
+            Token(TokenType.FLOAT, '').validate()
+        with self.assertRaises(ValueError):
+            Token(TokenType.FLOAT, 'a').validate()
+
+    def test_validate(self):
+        '''
+        Ensure that validate does not raise a `ValueError` when the token's
+        data is valid.
+        '''
+        Token(TokenType.NL, '\n').validate()
+        Token(TokenType.LT, '<').validate()
+        Token(TokenType.GT, '>').validate()
+        Token(TokenType.EQ, '=').validate()
+        Token(TokenType.PLUS, '+').validate()
+        Token(TokenType.MINUS, '-').validate()
+        Token(TokenType.DASH, '--').validate()
+        Token(TokenType.ARROW, '->').validate()
+        Token(TokenType.OBRACKET, '[').validate()
+        Token(TokenType.CBRACKET, ']').validate()
+        Token(TokenType.OPAREN, '(').validate()
+        Token(TokenType.CPAREN, ')').validate()
+        Token(TokenType.COMMA, ',').validate()
+
+        Token(TokenType.INTEGER, '1').validate()
+        Token(TokenType.INTEGER, '+1').validate()
+        Token(TokenType.INTEGER, '-1').validate()
+
+        Token(TokenType.FLOAT, '1').validate()
+        Token(TokenType.FLOAT, '+1').validate()
+        Token(TokenType.FLOAT, '-1').validate()
+
+        Token(TokenType.FLOAT, '1e2').validate()
+        Token(TokenType.FLOAT, '+1e2').validate()
+        Token(TokenType.FLOAT, '-1e2').validate()
+
+        Token(TokenType.FLOAT, '1e+2').validate()
+        Token(TokenType.FLOAT, '+1e+2').validate()
+        Token(TokenType.FLOAT, '-1e+2').validate()
+
+        Token(TokenType.FLOAT, '1e-2').validate()
+        Token(TokenType.FLOAT, '+1e-2').validate()
+        Token(TokenType.FLOAT, '-1e-2').validate()
+
+        Token(TokenType.STRING, '').validate()
+        Token(TokenType.STRING, '1').validate()
+        Token(TokenType.STRING, 'a').validate()
+        Token(TokenType.STRING, 'apple').validate()
+        Token(TokenType.STRING, '-1.0e-3').validate()
+
+    def test_str(self):
+        '''
+        Ensure that the __str__ method returns a properly formatted,
+        human-readable string.
+        '''
+        self.assertEqual('(NL, \'\\n\')', str(Token(TokenType.NL, '\n')))
+        self.assertEqual('(LT, \'<\')', str(Token(TokenType.LT, '<')))
+        self.assertEqual('(GT, \'>\')', str(Token(TokenType.GT, '>')))
+        self.assertEqual('(EQ, \'=\')', str(Token(TokenType.EQ, '=')))
+        self.assertEqual('(PLUS, \'+\')', str(Token(TokenType.PLUS, '+')))
+        self.assertEqual('(MINUS, \'-\')', str(Token(TokenType.MINUS, '-')))
+        self.assertEqual('(DASH, \'--\')', str(Token(TokenType.DASH, '--')))
+        self.assertEqual('(ARROW, \'->\')', str(Token(TokenType.ARROW, '->')))
+        self.assertEqual('(OBRACKET, \'[\')', str(Token(TokenType.OBRACKET, '[')))
+        self.assertEqual('(CBRACKET, \']\')', str(Token(TokenType.CBRACKET, ']')))
+        self.assertEqual('(OPAREN, \'(\')', str(Token(TokenType.OPAREN, '(')))
+        self.assertEqual('(CPAREN, \')\')', str(Token(TokenType.CPAREN, ')')))
+        self.assertEqual('(COMMA, \',\')', str(Token(TokenType.COMMA, ',')))
+
+        self.assertEqual('(INTEGER, \'1\')', str(Token(TokenType.INTEGER, '1')))
+        self.assertEqual('(INTEGER, \'+1\')', str(Token(TokenType.INTEGER, '+1')))
+        self.assertEqual('(INTEGER, \'-1\')', str(Token(TokenType.INTEGER, '-1')))
+
+        self.assertEqual('(FLOAT, \'1e2\')', str(Token(TokenType.FLOAT, '1e2')))
+        self.assertEqual('(FLOAT, \'+1e2\')', str(Token(TokenType.FLOAT, '+1e2')))
+        self.assertEqual('(FLOAT, \'-1e2\')', str(Token(TokenType.FLOAT, '-1e2')))
+
+        self.assertEqual('(STRING, \'\')', str(Token(TokenType.STRING, '')))
+        self.assertEqual('(STRING, \'+\')', str(Token(TokenType.STRING, '+')))
+        self.assertEqual('(STRING, \'apple\')', str(Token(TokenType.STRING, 'apple')))
+
+    def test_repr(self):
+        '''
+        Ensure that the __repr__ method returns a python-evaluable string.
+        '''
+        def check_repr(token):
+            got = eval(repr(token))
+            self.assertEqual(token.type, got.type)
+            self.assertEqual(token.data, got.data)
+
+        check_repr(Token(TokenType.NL, '\n'))
+        check_repr(Token(TokenType.LT, '<'))
+        check_repr(Token(TokenType.GT, '>'))
+        check_repr(Token(TokenType.EQ, '='))
+        check_repr(Token(TokenType.PLUS, '+'))
+        check_repr(Token(TokenType.MINUS, '-'))
+        check_repr(Token(TokenType.DASH, '--'))
+        check_repr(Token(TokenType.ARROW, '->'))
+        check_repr(Token(TokenType.OBRACKET, '['))
+        check_repr(Token(TokenType.CBRACKET, ']'))
+        check_repr(Token(TokenType.OPAREN, '('))
+        check_repr(Token(TokenType.CPAREN, ')'))
+        check_repr(Token(TokenType.COMMA, ','))
+
+        check_repr(Token(TokenType.INTEGER, '1'))
+        check_repr(Token(TokenType.INTEGER, '+1'))
+        check_repr(Token(TokenType.INTEGER, '-1'))
+
+        check_repr(Token(TokenType.FLOAT, '1e2'))
+        check_repr(Token(TokenType.FLOAT, '+1e2'))
+        check_repr(Token(TokenType.FLOAT, '-1e2'))
+
+        check_repr(Token(TokenType.STRING, ''))
+        check_repr(Token(TokenType.STRING, '+'))
+        check_repr(Token(TokenType.STRING, 'apple'))
